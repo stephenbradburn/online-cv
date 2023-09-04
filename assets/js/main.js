6b4ad5785463361b9c08d0484858136518575b62
stephenbradburn <143983393+stephenbradburn@users.noreply.github.com>
2023-09-04 14:40:33 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,21 @@
 jQuery(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
 
+    $(window).on('load', function() {
         $('.level-bar-inner').each(function() {
-        
             var itemWidth = $(this).data('level');
-            
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
         });
+    });
 
+    // Select elements with the class "project-title" and check their text content
+    $('.project-title').each(function() {
+        if ($(this).text() === "Working in Aerospace, Avionics, Defence, Pharmaceutical, Automotive, Component and Equipment Manufacturing") {
+            $(this).css('font-style', 'italic');
+        }
     });
 
 });
-
-let italicText = document.QuerySelector(".project-title");
-if(italicText.textContent == "Working in Aerospace, Avionics, Defence, Pharmaceutical, Automotive, Component and Equipment
-               Manufacturing")
-    italicText.style.fontStyle = 'italic';
-}
